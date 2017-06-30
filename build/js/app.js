152f8a3f4d47bdf37103cfb7064d1fd39c3d0b29
dennismuturia <dmuturia2922@gmail.com>
2017-06-30 18:25:00 +0300

    Changed my git

--- build/js/app.js
+++ build/js/app.js
@@ -3,38 +3,63 @@ exports.apiKey = "5ca7070c7fd71bf7c544f960a6b8406210f87166";
 
 },{}],2:[function(require,module,exports){
 var Gitname = require('./../js/scripts.js').nameModule;
-//var apikey = require('./../.env').apikey
 
 $(document).ready(function() {
   $('form#githubname').submit(function(event) {
     event.preventDefault();
     var uname = $('#username').val();
-    
+
     var nameObj = new Gitname(uname);
-    //console.log(nameObj.Apis());
-    nameObj.Apis();
+
+    console.log(nameObj.Apis());
+
+    //console.log();
+    nameObj.numRepos();
     //console.log(nameObj);
   });
 });
 
 },{"./../js/scripts.js":3}],3:[function(require,module,exports){
-var apikey = require('./../.env').apiKey
+var apikey = require('./../.env').apiKey;
 
 function Gitname(name) {
   this.name = name;
 }
 Gitname.prototype.Apis = function() {
   if (this.name != '') {
-    console.log("hello");
     $.get('https://api.github.com/users/' + this.name + '?access_token=' + apikey).then(function(response) {
-      console.log(response['login']);
 
+      $('.show-output').append('<img class="center2" src="' + response.avatar_url + '">');
+      $('.show-output').text();
+      $('.show-output1').append('<h3 class="center">Name: ' + ' ' + response.name + "</h3>");
+      $('.show-output1').text();
+      $('.show-output2').append('<h3 class="center">Followers: ' + ' ' + response.followers + "</h3>");
+      $('.show-output2').text();
+      $('.show-output3').append('<h3 class="center">Following:' + ' ' + response.following + "</h3>");
+      $('.show-output3').text();
+      $('.show-output4').append('<h3 class="center">public_repos:' + ' ' + response.public_repos + "</h3>");
+      $('.show-output4').text();
+
+      //console.log(response['avatar_url']);
     }).fail(function(error) {
       console.log(error.responseJSON.message);
     });
   } else {
     return "Invalid input";
   }
+};
+
+Gitname.prototype.numRepos = function() {
+
+  $.get('https://api.github.com/users/' + this.name + '/repos?access_token=' + apikey).then(function(response) {
+    for (var i = 0; i < response.length; i++) {
+    $('.rname').append('<h5>'+response[i].name+'</h5>');
+    $('.rdesc').append('<p>'+response[i].description+'</p>');
+    }
+  }).fail(function(error) {
+    console.log(error.responseJSON.message);
+  });
+
 };
 
 exports.nameModule = Gitname;
--- js/scripts-frontend.js
+++ js/scripts-frontend.js
@@ -1,14 +1,16 @@
 var Gitname = require('./../js/scripts.js').nameModule;
-//var apikey = require('./../.env').apikey
 
 $(document).ready(function() {
   $('form#githubname').submit(function(event) {
     event.preventDefault();
     var uname = $('#username').val();
-    
+
     var nameObj = new Gitname(uname);
-    //console.log(nameObj.Apis());
-    nameObj.Apis();
+
+    console.log(nameObj.Apis());
+
+    //console.log();
+    nameObj.numRepos();
     //console.log(nameObj);
   });
 });
